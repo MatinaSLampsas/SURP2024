b736bcec1364268b1d3f8c4cfc1f96d7d7b17949
Matina Lampsas <MSLampsa@calpoly.edu>
2024-07-14 14:35:39 -0700

    added adjusted data to file

--- docs/SHPI2.R
+++ docs/SHPI2.R
@@ -1,17 +1,17 @@
-setwd("C:/Users/kalis/OneDrive/Documents/Housing_research")
+# setwd("C:/Users/kalis/OneDrive/Documents/Housing_research")
 library(readxl)
 library(dplyr)
-data <- read_excel("San Luis Obispo County Sales 2019.xlsx", skip = 1) %>%
+data <- read_excel("data/San Luis Obispo County Sales 2019.xlsx", skip = 1) %>%
   mutate(Year = 2019)
 View(data)
 
 # Read the 2021 data and add a year column
-data2 <- read_excel("San Luis Obispo County Sales 2021.xlsx", skip = 1) %>%
+data2 <- read_excel("data/San Luis Obispo County Sales 2021.xlsx", skip = 1) %>%
   mutate(Year = 2021)
 View(data2)
 
 # Read the 2022 data and add a year column
-data3 <- read_excel("San Luis Obispo County Sales 2022.xlsx", skip = 1) %>%
+data3 <- read_excel("data/San Luis Obispo County Sales 2022.xlsx", skip = 1) %>%
   mutate(Year = 2022)
 View(data3)
 
@@ -19,7 +19,7 @@ combined_data <- bind_rows(data, data2, data3)
 View(combined_data)
 library(lubridate)
 
-hpi_data <- read_excel("ATNHPIUS06079A.xls", skip = 10)
+hpi_data <- read_excel("data/ATNHPIUS06079A.xls", skip = 10)
 hpi_data <- hpi_data %>%
   mutate(
     Year = year(ymd(observation_date)),  
@@ -34,3 +34,6 @@ adjusted_data <- combined_data %>%
 
 View(adjusted_data)
 print(colnames(data))
+
+# add the new csv to the data so that we can add the other variables in python
+write.csv(adjusted_data, file = "data/adjusted_data.csv")
